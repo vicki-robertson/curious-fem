a30ed7078a5351d421adbfa1920f5f27fd124140
vicki-robertson <135024584+vicki-robertson@users.noreply.github.com>
2023-12-18 23:37:04 +0100

    todos los estilos css terminados en movil y responsive. Tests con comentarios en inglés

--- test/favorite.test.js
+++ test/favorite.test.js
@@ -1,24 +1,24 @@
 const { loadFavorites, deleteFavorite } = require('./add_favorites');
 
-test('loadFavorites carga los favoritos correctamente', () => {
-    // Simula el almacenamiento local para la prueba
+test('loadFavorites loads the favorite facts correctly', () => {
+    // Simulates local storage for the test
     localStorage.setItem('favorites', JSON.stringify(['Fact1', 'Fact2']));
     const favoritesContainer = document.createElement('div');
 
-    // Llama a la función y verifica el resultado esperado
+    // Calls the function and verifies the expected result
     loadFavorites(favoritesContainer);
     expect(favoritesContainer.innerHTML).toContain('Fact1');
     expect(favoritesContainer.innerHTML).toContain('Fact2');
 });
 
-test('deleteFavorite elimina un favorito correctamente', () => {
-    // Simula el almacenamiento local para la prueba
+test('deleteFavorite eliminates a favorite fact correctly', () => {
+    // Simulates local storage of the test
     localStorage.setItem('favorites', JSON.stringify(['Fact1', 'Fact2']));
     
-    // Llama a la función para eliminar un favorito
+    // Calls the function to eliminate a favorite
     deleteFavorite(0);
     
-    // Verifica que el favorito se haya eliminado correctamente
+    // Verifies that the favorite has been correctly eliminated
     const storedFavorites = JSON.parse(localStorage.getItem('favorites'));
     expect(storedFavorites).toHaveLength(1);
     expect(storedFavorites[0]).toBe('Fact2');
